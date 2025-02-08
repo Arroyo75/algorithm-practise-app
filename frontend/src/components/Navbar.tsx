1ef4bf1874fc0f04502805541d83013279883f0a
jan <jszczepanski75@gmail.com>
2025-02-08 22:36:30 +0100

    arguablly better navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,21 +1,83 @@
-import { Box, Flex, Button, Heading } from '@chakra-ui/react'
-import { Link as RouterLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { Container, Flex, Button, HStack, Text} from '@chakra-ui/react';
 
 const Navbar = () => {
-  return (
-    <Box px={4} bg="gray.100">
-      <Flex h={16} alignItems="center" justifyContent="space-between">
-        <Heading size="md" as={RouterLink} to="/">
-          CodeChallenges
-        </Heading>
-        <Flex gap={4}>
-          <Button as={RouterLink} to="/">
-            Challenges
-          </Button>
-        </Flex>
-      </Flex>
-    </Box>
-  )
+
+    return (
+        <Container maxW={"1140px"} px={4}>
+            <Flex
+                h={16}
+                alignItems={"center"}
+                justifyContent={"space-between"}
+                flexDir={{
+                    base:"column",
+                    md:"row"
+                }}
+                gap={{
+                    md: 12,
+                    base: 1
+                }}
+            >
+                <Text
+                    fontSize={{ base:"25", md:"30"}}
+                    fontWeight={"bold"}
+                    textTransform={"uppercase"}
+                    textAlign={"center"}
+                    bgGradient={"linear(to-r, orange.400, red.500)"}
+                    bgClip={"text"}
+                >
+                    ALGORITHMS
+                </Text>
+                <HStack spacing={8} alignItems={"center"}>
+                    <Button
+                        variant={"link"}
+                        fontSize={{ base:"16", sm:"18"}}
+                        fontWeight={"bold"}
+                        textAlign={"center"}
+                        bg={"gray.900"}
+                        bgGradient={"linear(to-r, orange.400, red.500)"}
+                        bgClip={"text"}
+                        mt={{base: 3, md: 0}}
+                        _hover={{bgGradient: "linear(to-r, yellow.400, pink.500)",
+                            transform: "translateY(-5px)"}}
+                        _active={{bg: "gray.900"}}
+                    >
+                        Register
+                    </Button>
+                    <Button
+                        variant={"link"}
+                        fontSize={{ base:"16", sm:"18"}}
+                        fontWeight={"bold"}
+                        textAlign={"center"}
+                        bg={"gray.900"}
+                        bgGradient={"linear(to-r, orange.400, red.500)"}
+                        bgClip={"text"}
+                        mt={{base: 3, md: 0}}
+                        _hover={{bgGradient: "linear(to-r, yellow.400, pink.500)",
+                            transform: "translateY(-5px)"}}
+                        _active={{bg: "gray.900"}}
+                    >
+                        Log in
+                    </Button>
+                    <Button
+                        variant={"link"}
+                        fontSize={{ base:"16", sm:"18"}}
+                        fontWeight={"bold"}
+                        textAlign={"center"}
+                        bg={"gray.900"}
+                        bgGradient={"linear(to-r, orange.400, red.500)"}
+                        bgClip={"text"}
+                        _hover={{bgGradient: "linear(to-r, yellow.400, pink.500)",
+                            transform: "translateY(-5px)"}}
+                        _active={{bg: "gray.900"}}
+                    >
+                        Log out
+                    </Button>
+                </HStack>
+            </Flex>
+
+        </Container>
+    )
 }
 
 export default Navbar;
