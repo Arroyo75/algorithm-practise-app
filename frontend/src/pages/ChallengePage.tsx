8aa0b805f60aab92e2d8e12b3daf91ca9ca0ed13
jan <jszczepanski75@gmail.com>
2025-02-12 23:36:15 +0100

    ChallangePage

---
+++ frontend/src/pages/ChallengePage.tsx
@@ -0,0 +1,49 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'reac-router-dom';
+import { Box, Grid, Button, Text, VStack } from '@chakra-ui/react';
+import Editor, { loader } from "@monaco-editor/react";
+
+loader.config({
+  paths: {
+    vs: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.45.0/min/vs'
+  }
+});
+
+const ChallengePage = () => {
+  const { id } = useParams();
+  const [code, setCode] = useState('');
+  const [isEditorReady, setIsEditorReady] = useState(false);
+
+  const handleEditorDidMount = () => {
+    setIsEdtiorReady(true);
+  };
+
+  return (
+    <Box p={4}>
+      <Editor
+        height="70vh"
+        defaultLanguage='javascript'
+        value={code}
+        onChange={(value) => setCode(value || '')}
+        theme="vs-dark"
+        onMount={handleEditorDidMount}
+        loading={<Text>Loading editor...</Text>}
+        options={{
+          minimap: { enabled: false },
+          fontSize: 14,
+          wordWrap: 'on',
+          automaticLayout: true
+        }}
+      />
+      <Button
+        mt={4}
+        colorScheme="green"
+        isDisabled={!isEditorReady}
+      >
+        Run Code
+      </Button>
+    </Box>
+  );
+};
+
+export default ChallengePage;
